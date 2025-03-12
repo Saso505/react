a533851c3473ebe4b5d7484e0e6988cb4a04a3ca
Saso505 <osamasalma921@gmail.com>
2025-03-12 17:55:23 +0200

    Fix homepage path

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   return (
     <>
       <USerContextProvider>
-        <Router></Router>
+
         <RouterProvider router={routers}></RouterProvider>
         <Toaster></Toaster>
       </USerContextProvider>
